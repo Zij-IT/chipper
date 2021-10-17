ea47f1393d76a154309ec64690539bfb8965c321
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-10-17 17:51:14 +0200

    Chip8: Implemented array-based stack

--- src/chip/stack.rs
+++ src/chip/stack.rs
@@ -1,5 +1,16 @@
-pub struct Stack(Vec<u8>);
+pub struct Stack {
+    stack: [u16; 16],
+    stack_ptr: usize,
+}
 
 impl Stack {
+    pub fn pop(&mut self) -> u16 {
+        self.stack_ptr -= 1;
+        self.stack[self.stack_ptr]
+    }
 
+    pub fn push(&mut self, byte: u16) {
+        self.stack[self.stack_ptr] = byte;
+        self.stack_ptr += 1;
+    }
 }
