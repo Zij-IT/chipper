3e67ba34df5ee74f6c2ee13905ed819ba684aabc
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-10-17 17:52:18 +0200

    Chip8: Implemented indexing into registers using u8

--- src/chip/register.rs
+++ src/chip/register.rs
@@ -1,5 +1,24 @@
-pub struct Registers([u8; 16]);
+use std::ops::Index;
+use std::ops::IndexMut;
+
+pub struct Registers([u8; 15]);
 
 impl Registers {
 
+}
+
+impl Index<u8> for Registers {
+    type Output = u8;
+
+    fn index(&self, idx: u8) -> &Self::Output {
+        debug_assert!(idx < 16);
+        &self.0[idx as usize]
+    }
+}
+
+impl IndexMut<u8> for Registers {
+    fn index_mut(&mut self, idx: u8) -> &mut Self::Output {
+        debug_assert!(idx < 16);
+        &mut self.0[idx as usize]
+    }
 }
