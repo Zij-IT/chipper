726378e2f9d28f123fff71c438776eef71f21e1b
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-10-17 17:44:52 +0200

    Chip8: Added basic display functionality

--- src/chip/display.rs
+++ src/chip/display.rs
@@ -1,5 +1,11 @@
 pub struct Display([[u8; 32]; 64]);
 
 impl Display {
+    pub fn new() -> Self {
+        Self([[0; 32]; 64])
+    }
 
+    pub fn clear(&mut self) {
+        *self = Self::new();
+    }
 }
