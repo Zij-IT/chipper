5d4568fe8d121e565347d2b86a9f66d3e8d16c5c
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-10-24 00:01:41 +0200

    Chip8: Moved SDL2 code into Wrapper class

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ mod sdl2_wrapper;
 use chip::Chip8;
 
 use anyhow::Result;
-use sdl2_wrapper::Sdl2Error;
-use sdl2_wrapper::*;
+use sdl2_wrapper::Sdl2Wrapper;
 use std::time::Duration;
 
 const SLEEP_DURATION: Duration = Duration::from_millis(2);
@@ -18,24 +17,21 @@ const SCALE: usize = 20;
 fn main() -> Result<()> {
     let rom = include_bytes!("../test_opcode.ch8");
 
-    let sdl_context = create_sdl_context()?;
-    let mut canvas = setup_canvas(&sdl_context)?;
-    let mut event_pump = sdl_context
-        .event_pump()
-        .map_err(Sdl2Error::UnableToBuildEventPump)?;
-
+    let mut sdl = Sdl2Wrapper::new()?;
     let mut chip8 = Chip8::new();
     chip8.load_rom(rom)?;
 
-    let mut quit = false;
-    while !quit {
-        let (quit_signal, keys) = poll_input(&mut event_pump);
-        quit = quit_signal;
+    loop {
+        let (quit_signal, keys) = sdl.poll_input();
 
         chip8.cycle(keys)?;
-        draw_on_canvas(&mut canvas, chip8.get_frame_buffer())?;
+        sdl.draw_on_canvas(chip8.get_frame_buffer())?;
 
         std::thread::sleep(SLEEP_DURATION);
+
+        if quit_signal {
+            break;
+        }
     }
 
     Ok(())
--- src/sdl2_wrapper.rs
+++ src/sdl2_wrapper.rs
@@ -2,82 +2,103 @@ use super::chip::FrameBuffer;
 use super::{CHIP8_HEIGHT, CHIP8_WIDTH, SCALE};
 
 use anyhow::Result;
+
 use sdl2::keyboard::Scancode;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::EventPump;
+use sdl2::Sdl;
 
-pub fn create_sdl_context() -> Result<sdl2::Sdl> {
-    sdl2::init().map_err(|e| Sdl2Error::UnableToBuildSdl(e).into())
+pub struct Sdl2Wrapper {
+    canvas: Canvas<Window>,
+    event_pump: EventPump,
 }
 
-pub fn setup_canvas(sdl_context: &sdl2::Sdl) -> Result<Canvas<Window>> {
-    let video = sdl_context
-        .video()
-        .map_err(Sdl2Error::UnableToBuildVideo)?;
+impl Sdl2Wrapper {
+    pub fn new() -> Result<Self> {
+        let sdl_context = Self::create_sdl_context()?;
+        let canvas = Self::setup_canvas(&sdl_context)?;
+        let event_pump = sdl_context
+            .event_pump()
+            .map_err(Sdl2Error::UnableToBuildEventPump)?;
 
-    let window = video
-        .window(
-            "Chipper: Chip8 Emulator",
-            (SCALE * CHIP8_WIDTH) as u32,
-            (SCALE * CHIP8_HEIGHT) as u32,
-        )
-        .position_centered()
-        .opengl()
-        .build()?;
-
-    let mut canvas = window.into_canvas().build()?;
-
-    canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
-
-    Ok(canvas)
-}
-
-pub fn draw_on_canvas(canvas: &mut Canvas<Window>, buffer: &FrameBuffer) -> Result<()> {
-    for (y, row) in buffer.iter().enumerate() {
-        for (x, &col) in row.iter().enumerate() {
-            let x = (x * SCALE) as u32;
-            let y = (y * SCALE) as u32;
-
-            let color = if col == 0 {
-                sdl2::pixels::Color::RGB(0, 0, 0)
-            } else {
-                sdl2::pixels::Color::RGB(0xFF, 0xFF, 0xFF)
-            };
-
-            canvas.set_draw_color(color);
-            canvas
-                .fill_rect(sdl2::rect::Rect::new(
-                    x as i32,
-                    y as i32,
-                    SCALE as u32,
-                    SCALE as u32,
-                ))
-                .map_err(Sdl2Error::UnableToDraw)?;
-        }
+        Ok(Self {
+            canvas,
+            event_pump,
+        })
     }
 
-    canvas.present();
-    Ok(())
-}
+    pub fn draw_on_canvas(&mut self, buffer: &FrameBuffer) -> Result<()> {
+        for (y, row) in buffer.iter().enumerate() {
+            for (x, &col) in row.iter().enumerate() {
+                let x = (x * SCALE) as u32;
+                let y = (y * SCALE) as u32;
 
-pub fn poll_input(event_pump: &mut sdl2::EventPump) -> (bool, [bool; 16]) {
-    let mut pressed_keys = [false; 16];
+                let color = if col == 0 {
+                    sdl2::pixels::Color::RGB(0, 0, 0)
+                } else {
+                    sdl2::pixels::Color::RGB(0xFF, 0xFF, 0xFF)
+                };
 
-    let quit = event_pump
-        .poll_iter()
-        .any(|event| matches!(event, sdl2::event::Event::Quit { .. }));
+                self.canvas.set_draw_color(color);
+                self.canvas
+                    .fill_rect(sdl2::rect::Rect::new(
+                        x as i32,
+                        y as i32,
+                        SCALE as u32,
+                        SCALE as u32,
+                    ))
+                    .map_err(Sdl2Error::UnableToDraw)?;
+            }
+        }
 
-    event_pump
-        .keyboard_state()
-        .pressed_scancodes()
-        .filter_map(translate_scancode)
-        .for_each(|key| {
-            pressed_keys[key as usize] = true;
-        });
+        self.canvas.present();
+        Ok(())
+    }
 
-    (quit, pressed_keys)
+    pub fn poll_input(&mut self) -> (bool, [bool; 16]) {
+        let mut pressed_keys = [false; 16];
+
+        let quit = self.event_pump
+            .poll_iter()
+            .any(|event| matches!(event, sdl2::event::Event::Quit { .. }));
+
+        self.event_pump
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(translate_scancode)
+            .for_each(|key| {
+                pressed_keys[key as usize] = true;
+            });
+
+        (quit, pressed_keys)
+    }
+
+    fn create_sdl_context() -> Result<Sdl> {
+        sdl2::init().map_err(|e| Sdl2Error::UnableToBuildSdl(e).into())
+    }
+
+    fn setup_canvas(sdl_context: &sdl2::Sdl) -> Result<Canvas<Window>> {
+        let video = sdl_context.video().map_err(Sdl2Error::UnableToBuildVideo)?;
+
+        let window = video
+            .window(
+                "Chipper: Chip8 Emulator",
+                (SCALE * CHIP8_WIDTH) as u32,
+                (SCALE * CHIP8_HEIGHT) as u32,
+            )
+            .position_centered()
+            .opengl()
+            .build()?;
+
+        let mut canvas = window.into_canvas().build()?;
+
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+
+        Ok(canvas)
+    }
 }
 
 fn translate_scancode(key: Scancode) -> Option<u8> {
