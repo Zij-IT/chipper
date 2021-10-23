02f2966bb1e66d40629622d60005cb04fa470698
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-10-23 12:56:28 +0200

    Chip8: Prototyping finished. Migrating to sdl2

--- src/chip/display.rs
+++ src/chip/display.rs
@@ -1,13 +1,38 @@
-#[derive(PartialEq, Eq, Debug)]
-pub struct Display([[u8; 64]; 32]);
+const SCALE: usize = 20;
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+
+pub struct Display {
+    display: [[u8; WIDTH]; HEIGHT],
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
+}
 
 impl Display {
-    pub fn new() -> Self {
-        Self([[0; 64]; 32])
+    pub fn new(sdl_context: &sdl2::Sdl) -> Self {
+        let video = sdl_context.video().unwrap();
+        let window = video.
+                window(
+                        "title",
+                        (SCALE * WIDTH) as u32,
+                        (SCALE * HEIGHT) as u32,
+                )
+                .position_centered()
+                .opengl()
+                .build()
+                .unwrap();
+        let canvas = window.into_canvas().build().unwrap();
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+
+        Self {
+            display: [[0; 64]; 32],
+            canvas,
+        }
     }
 
     pub fn clear(&mut self) {
-        *self = Self::new();
+        self.display = [[0; 64]; 32];
     }
 
     pub fn draw_byte(&mut self, byte: u8, x: u8, y: u8) -> bool {
@@ -22,11 +47,11 @@ impl Display {
             }
 
             let bit = (byte & 0x80) >> 7;
-            let prev_bit = self.0[coord_y][coord_x];
+            let prev_bit = self.display[coord_y][coord_x];
 
-            self.0[coord_y][coord_x] ^= bit;
+            self.display[coord_y][coord_x] ^= bit;
 
-            erased = erased || (prev_bit == 1 && self.0[coord_y][coord_x] == 0);
+            erased = erased || (prev_bit == 1 && self.display[coord_y][coord_x] == 0);
             coord_x += 1;
             byte <<= 1;
         }
@@ -35,19 +60,38 @@ impl Display {
     }
 
     pub fn frame_buffer(&self) -> &[[u8; 64]; 32] {
-        &self.0
+        &self.display
     }
 
     fn width(&self) -> usize {
-        self.0[0].len()
+        WIDTH
     }
 
     fn height(&self) -> usize {
-        self.0.len()
+        HEIGHT
     }
 
     #[cfg(test)]
-    pub fn new_filled() -> Self {
-        Self([[0; 64]; 32])
+    pub fn new_filled(sdl_context: &sdl2::Sdl) -> Self {
+        let video = sdl_context.video().unwrap();
+        let window = video.
+                window(
+                        "title",
+                        (SCALE * WIDTH) as u32,
+                        (SCALE * HEIGHT) as u32,
+                )
+                .position_centered()
+                .opengl()
+                .build()
+                .unwrap();
+        let canvas = window.into_canvas().build().unwrap();
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+
+        Self {
+            display: [[0xFF; 64]; 32],
+            canvas,
+        }
     }
 }
