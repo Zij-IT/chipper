545d6ab4b2be37b2b95d994acf1ddf1fc873566a
Zij-IT <elijah.reed@hartvigsen.xyz>
2021-10-24 17:16:53 +0200

    Chip8: Added ability to accept path arg for rom

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod sdl2_wrapper;
 use chip::Chip8;
 
 use anyhow::Result;
+use anyhow::Error;
 use sdl2_wrapper::Sdl2Wrapper;
 
 const CHIP8_HEIGHT: usize = 32;
@@ -13,11 +14,14 @@ const CHIP8_WIDTH: usize = 64;
 const SCALE: usize = 20;
 
 fn main() -> Result<()> {
-    let rom = include_bytes!("../games/hidden.ch8");
+    let path = std::env::args()
+        .nth(1)
+        .ok_or_else(|| Error::msg("No rom path provided. Exiting."))?;
+    let rom = std::fs::read(path)?;
 
     let mut sdl = Sdl2Wrapper::new()?;
     let mut chip8 = Chip8::new();
-    chip8.load_rom(rom)?;
+    chip8.load_rom(&rom)?;
     chip8.run(&mut sdl)?;
 
     Ok(())
